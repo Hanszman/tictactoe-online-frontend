33c124ff4686c55954f0cfe91c52118ff27d3040
Hanszman <victor.hanszman@hotmail.com>
2024-06-12 08:11:44 -0300

    implementing signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import { StreamChat } from 'stream-chat'
+import Cookies from 'universal-cookie';
 
 function App() {
+  const cookies = new Cookies();
+  const api_key = 'b52r7nyk5b8v';
+  const token = cookies.get('token');
+  const serverClient = StreamChat.getInstance(api_key);
+  const client = StreamChat.getInstance(api_key);
+  if (token) {
+    client.connectUser(
+      {
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        firstName: cookies.get('firstName'),
+        lastName: cookies.get('lastName'),
+        hashedPassword: cookies.get('hashedPassword'),
+      },
+      token
+    ).then((user) => {
+      console.log(user);
+    });
+  }
   return (
     <div className="App">
       <Signup/>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
+import Axios from 'axios';
+import Cookies from 'universal-cookie';
 
 function Signup() {
+    const cookies = new Cookies();
     const [user, setUser] = useState(null);
     const signUp = () => {
-
+        Axios.post('http://localhost:3001/signup', user).then(
+            res => {
+                const {token, userId, firstName, lastName, username, hashedPassword} = res.data;
+                cookies.set('token', token);
+                cookies.set('userId', userId);
+                cookies.set('firstName', firstName);
+                cookies.set('lastName', lastName);
+                cookies.set('username', username);
+                cookies.set('hashedPassword', hashedPassword);
+            },
+            err => {
+                console.log(err);
+            }
+        );
     };
 
     return (
